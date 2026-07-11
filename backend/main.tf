f9b7e2362d321c42248b8ad25cedf5f0fe0563d6
fred <frederico.almeidamoreira@gmail.com>
2026-07-11 20:54:34 -0300

    fix:Makefile

--- backend/main.tf
+++ backend/main.tf
@@ -1,7 +1,14 @@
-module "lambda_backend" {
-  source = "./modules/lambda"
+#1. Orquestra a criação do IAM
+module "iam_backend" {
+  source       = "./modules/iam"
+  project_name = var.project_name
+  workspace = terraform.workspace
+}
 
+#2. Lambda consome o ARN criado pelo módulo IAM
+module "lambda_backend" {
+  source       = "./modules/lambda"
   project_name = var.project_name
   workspace    = terraform.workspace
-  iam_role_arn = var.iam_role_arn
+  iam_role_arn = module.iam_backend.lambda_role_arn
 }
